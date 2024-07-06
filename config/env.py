5c55fc380201848d828f21cdc2d1747aee36adec
WojciechKubak <wojciechkubak99@gmail.com>
2024-07-06 14:55:28 +0200

    Add ENV setup

--- config/env.py
+++ config/env.py
@@ -1,4 +1,11 @@
 from dotenv import load_dotenv
 
-
 load_dotenv()
+
+
+def env_to_list(env: str | None) -> list[str]:
+    return env.split(",") if env else []
+
+
+def env_to_bool(env: str | None) -> bool:
+    return str(env).upper() in ["TRUE", "1", "YES"] if env else False
