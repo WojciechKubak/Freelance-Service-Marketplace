2270778a899489e7f644286c883e0a35218d7220
WojciechKubak <wojciechkubak99@gmail.com>
2024-07-07 11:52:13 +0200

    Add production docker-compose

--- config/django/base.py
+++ config/django/base.py
@@ -87,9 +87,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB", "db"),
-        "USER": os.environ.get("POSTGRES_USER", "user"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
+        "NAME": "db",
+        "USER": "user",
+        "PASSWORD": "password1234",
         "HOST": "postgres",
         "PORT": "5432",
     }
--- config/django/production.py
+++ config/django/production.py
@@ -3,7 +3,18 @@ from .base import *  # noqa
 import os
 
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "db"),
+        "USER": os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password1234"),
+        "HOST": os.environ.get("POSTGRES_HOST", "postgres"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+    }
+}
+
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 ALLOWED_HOSTS = env_to_list(os.environ.get("DJANGO_ALLOWED_HOSTS"), [])
 
