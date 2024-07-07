aa02ab81798cf2eb25e881bdd4d01685bbbbcfdb
WojciechKubak <wojciechkubak99@gmail.com>
2024-07-07 08:43:15 +0200

    Add DB setup

--- config/django/test.py
+++ config/django/test.py
@@ -1,3 +1,4 @@
+from config.env import BASE_DIR
 from .base import *  # noqa
 
 
@@ -8,5 +9,12 @@ PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    }
+}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
