08adbb5ca8615077a4e71505c6c648070500e078
WojciechKubak <wojciechkubak99@gmail.com>
2024-07-07 11:20:05 +0200

    Django SSL setup

--- config/django/production.py
+++ config/django/production.py
@@ -1,4 +1,4 @@
-from config.env import env_to_list
+from config.env import env_to_list, env_to_bool
 from .base import *  # noqa
 import os
 
@@ -6,3 +6,11 @@ import os
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = env_to_list(os.environ.get("DJANGO_ALLOWED_HOSTS"), [])
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
+SECURE_SSL_REDIRECT = env_to_bool(os.environ.get("SECURE_SSL_REDIRECT"), True)
+# https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
+SECURE_CONTENT_TYPE_NOSNIFF = env_to_bool(
+    os.environ.get("SECURE_CONTENT_TYPE_NOSNIFF"), True
+)
