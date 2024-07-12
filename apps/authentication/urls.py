1c6c856f7656ac9194ccd2971c14ab1ede3a1ca4
WojciechKubak <wojciechkubak99@gmail.com>
2024-07-12 14:46:47 +0200

    Auth api

---
+++ apps/authentication/urls.py
@@ -0,0 +1,8 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path
+
+
+urlpatterns = [
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+]
