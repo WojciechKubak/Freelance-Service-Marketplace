d38dc222456d0717f61c78d6362d3f721283d59e
WojciechKubak <wojciechkubak99@gmail.com>
2024-07-13 12:55:50 +0200

    Move routes to api app

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,7 +1,8 @@
 from apps.api.apis import HealthCheckAPI
-from django.urls import path
-
+from django.urls import path, include
 
 urlpatterns = [
-    path("health/", HealthCheckAPI.as_view(), name="health-check"),
+    path("health/", HealthCheckAPI.as_view(), name="health_check"),
+    path("auth/", include("apps.authentication.urls")),
+    path("users/", include("apps.users.urls")),
 ]
