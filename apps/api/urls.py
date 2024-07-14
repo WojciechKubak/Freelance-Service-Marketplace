6d15bade1c0b99e73eafddbff54b285dcfe96411
WojciechKubak <wojciechkubak99@gmail.com>
2024-07-14 10:31:01 +0200

    Remove health check api

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,8 +1,6 @@
-from apps.api.apis import HealthCheckAPI
 from django.urls import path, include
 
 urlpatterns = [
-    path("health/", HealthCheckAPI.as_view(), name="health_check"),
     path("auth/", include("apps.authentication.urls")),
     path("users/", include("apps.users.urls")),
 ]
