14ce37fc92b64d9c3c0839f7f7de53b7359545ab
WojciechKubak <wojciechkubak99@gmail.com>
2024-07-20 11:23:26 +0200

    Set activation email lifespan

--- config/settings/email_sending.py
+++ config/settings/email_sending.py
@@ -7,14 +7,16 @@ EMAIL_SENDING_STRATEGY = env_to_enum(
     EmailSendingStrategy, os.environ.get("EMAIL_SENDING_STRATEGY", "console")
 )
 
+EMAIL_ACTIVATION_TIMEOUT = int(os.environ.get("EMAIL_ACTIVATION_TIMEOUT", 60 * 60 * 24))
+
 if EMAIL_SENDING_STRATEGY == EmailSendingStrategy.CONSOLE:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 if EMAIL_SENDING_STRATEGY == EmailSendingStrategy.SMTP:
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = os.environ.get("EMAIL_HOST")
-    EMAIL_USE_TLS = env_to_bool(os.environ.get("EMAIL_USE_TLS"))
-    EMAIL_USE_SSL = env_to_bool(os.environ.get("EMAIL_USE_SSL"))
+    EMAIL_USE_TLS = env_to_bool(os.environ.get("EMAIL_USE_TLS"), False)
+    EMAIL_USE_SSL = env_to_bool(os.environ.get("EMAIL_USE_SSL"), True)
     EMAIL_PORT = os.environ.get("EMAIL_PORT")
     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
