bba793887dac7c14427206ad06d8a8187b59c6c4
Wojciech Kubak <wojciechkubak99@gmail.com>
2024-08-25 14:05:17 +0200

    Add Swagger UI

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,9 +1,30 @@
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
+
+# https://drf-spectacular.readthedocs.io/en/latest/
+schema_patterns = [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="api:schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "schema/redoc/",
+        SpectacularRedocView.as_view(url_name="api:schema"),
+        name="redoc",
+    ),
+]
 
 urlpatterns = [
     path("auth/", include("apps.authentication.urls")),
     path("users/", include("apps.users.urls")),
     path("categorization/", include("apps.categorization.urls")),
     path("consultations/", include("apps.consultations.urls")),
+    path("", include(schema_patterns)),
 ]
