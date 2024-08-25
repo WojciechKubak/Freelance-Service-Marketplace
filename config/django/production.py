d3b7f7b51143b7f28c5a82573c364d64d9616091
Wojciech Kubak <wojciechkubak99@gmail.com>
2024-08-25 11:38:27 +0200

    HTTPS config

--- config/django/production.py
+++ config/django/production.py
@@ -1,7 +1,18 @@
-from config.env import env_to_list
+from config.env import env_to_list, env_to_bool  # noqa
 from .base import *  # noqa
 import os
 
+
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
+ALLOWED_HOSTS = env_to_list(os.environ.get("DJANGO_ALLOWED_HOSTS"), [])
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ORIGIN_WHITELIST = env_to_list(os.environ.get("DJANGO_CORS_ORIGIN_WHITELIST"), [])
+
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# SECURE_SSL_REDIRECT = env_to_bool(os.environ.get("DJANGO_SECURE_SSL_REDIRECT"), True)
+# SECURE_CONTENT_TYPE_NOSNIFF = os.environ.get("DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", True)
 
 DATABASES = {
     "default": {
@@ -13,7 +24,3 @@ DATABASES = {
         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
-
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-
-ALLOWED_HOSTS = env_to_list(os.environ.get("DJANGO_ALLOWED_HOSTS"), [])
